7b4b8fb746f78f2f2cba85a7da137322d0c7dc22
BRYAN JAMES PAIVA DOS SANTOS <143773176+Bryan-dev22@users.noreply.github.com>
2023-09-14 13:21:37 -0300

    Adding folders

--- aula_curso_refatorando/app.py
+++
@@ -1,5 +0,0 @@
-nome = "Bryan"
-idade = "22"
-gosto_de = "programar"
-
-print(nome + " tem " + idade + " anos e gosta de " + gosto_de)
---
+++ aula_curso_refatorando/aula_1_e_2/app1.py
@@ -0,0 +1,13 @@
+nome1 = "Bryan" # String
+idade1 = 22 # Number
+altura1 = 1.91 # Float
+eh_homem = true # Bool
+gosto_de1 = "programar" # String
+
+
+nome2 = "Letícia"
+idade2 = "23"
+gosto_de2 = "enfermagem"
+
+print(nome1 + " tem " + idade1 + " anos e gosta de " + gosto_de1)
+print(f"A {nome2} tem {idade2} e gosta de {gosto_de2}") # Este é o método mais fácil!!
